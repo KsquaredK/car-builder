0c765b7893438bfeef76e61e810b77ea876b804e
KsquaredK <ksquaredkelly@gmail.com>
2021-05-07 16:47:39 -0400

    in progress -  construction queue and price quotes

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,13 +1,12 @@
 import { getPaintColors, getInteriors, getTechPacks, getWheels, getOrders } from "./database.js"
 console.log("Orders module initialized...")
 
+const paintColors = getPaintColors()
+const interiors = getInteriors()
+const techPacks = getTechPacks()
+const wheels = getWheels()
+
 const buildOrderListItem = (order) => {
-    const paintColors = getPaintColors()
-    const interiors = getInteriors()
-    const techPacks = getTechPacks()
-    const wheels = getWheels()
-
-
     const foundPaintColor = paintColors.find(
         (paintColor) => {
             return paintColor.id === order.paintColorId
@@ -38,15 +37,53 @@ const buildOrderListItem = (order) => {
 Order #${order.id} cost ${costString}, and was placed on ${order.timestamp}</li>`
 }
 
+/* 
+    Make sure there is a foreign key for each options type on the new object.
+Make sure there is a new primary key value that is 1 greater than the current highest one.
+Make sure there is a timestamp on the order.
+ */
+/* 
+Make sure you clear out the state that was being used to store the user options once the order state is created.
+Trigger the stateChanged custom events and have the main module listen for it and regenerate all of the HTML.
+Once regenerated, the new order should appear in the DOM. Example:
+Order #1 was placed on 16895837301 
+*/
+
 export const Orders = () => {
     console.log("Orders function invoked...")
+
     const orders = getOrders()
+    let totalCost = 0
+    const arrayOfHTMLStrings = orders.map(
+            (order) => {
+                const foundPaintColor = paintColors.find(paintColor => paintColor.id === order.paintColorId)
+                totalCost += foundPaintColor.price
+                const foundInterior = interiors.find(interior => interior.id === order.interiorId)
+                totalCost += foundInterior.price
+                const foundTechPack = techPacks.find(techPack => techPack.id === order.techPackId)
+                totalCost += foundTechPack.price
+                const foundWheel = wheels.find(wheel => wheel.id === order.wheelId)
+                totalCost += foundWheel.price
+
+            })
+        /* 
+         
+            const foundPaintColor = paintColors.find(
+                (paintColorObj) => {
+                    if (paintColorObj.id === paintColor.id) {
+                        return true
+                    }
+                }
+            ) */
+
+
+
     let html = "<ul>"
-    const ArrayoflistItems = orders.map((orderObj) => {
+    const arrayOfHTML = orders.map((orderObj) => {
         const singularListItemHtml = buildOrderListItem(orderObj)
         return singularListItemHtml
     })
-    html += ArrayoflistItems.join("")
+    html += arrayOfHTMLStrings.join("")
     html += "</ul>"
 
     return html
--- scripts/database.js
+++ scripts/database.js
@@ -1,41 +1,42 @@
 const database = {
-        paintColors: [
-            { id: 1, color: "Silver", price: 20.99 },
-            { id: 2, color: "Midnight Blue", price: 20.99 },
-            { id: 3, color: "Firebrick Red", price: 99.99 },
-            { id: 4, color: "Spring Green", price: 20.99 }
-        ],
-        interiors: [
-            { id: 1, colorType: "Beige Fabric", price: 10.99 },
-            { id: 2, colorType: "Charcoal Fabric", price: 10.99 },
-            { id: 3, colorType: "White Leather", price: 249.99 },
-            { id: 4, colorType: "Black Leather", price: 219.99 }
-        ],
-        techPacks: [
-            { id: 1, type: "Basic", price: 299.99 },
-            { id: 2, type: "Navigation Package (includes integrated navigation controls)", price: 699.99 },
-            { id: 3, type: "Visibility Package (includes side and rear cameras)", price: 699.99 },
-            { id: 4, type: "Ultra Package (includes navigation and visibility packages)", price: 999.99 }
-        ],
-        wheels: [
-            { id: 1, sizeType: "17-inch Pair Radial", price: 199.99 },
-            { id: 2, sizeType: "17-inch Pair Radial Black", price: 269.99 },
-            { id: 3, sizeType: "18-inch Pair Spoke Silver", price: 349.99 },
-            { id: 4, sizeType: "18-inch Pair Spoke Black", price: 379.99 },
-        ],
-        customOrders: [{
-            id: 1,
-            paintColorId: 3,
-            interiorId: 2,
-            techPackId: 3,
-            wheelId: 4,
-            timestamp: 1620059468223,
-        }],
-        //object that holds transient state (current user's input)
-        orderBuilder: {}
+    paintColors: [
+        { id: 1, color: "Silver", price: 20.99 },
+        { id: 2, color: "Midnight Blue", price: 20.99 },
+        { id: 3, color: "Firebrick Red", price: 99.99 },
+        { id: 4, color: "Spring Green", price: 20.99 }
+    ],
+    interiors: [
+        { id: 1, colorType: "Beige Fabric", price: 10.99 },
+        { id: 2, colorType: "Charcoal Fabric", price: 10.99 },
+        { id: 3, colorType: "White Leather", price: 249.99 },
+        { id: 4, colorType: "Black Leather", price: 219.99 }
+    ],
+    techPacks: [
+        { id: 1, type: "Basic", price: 299.99 },
+        { id: 2, type: "Navigation Package (includes integrated navigation controls)", price: 699.99 },
+        { id: 3, type: "Visibility Package (includes side and rear cameras)", price: 699.99 },
+        { id: 4, type: "Ultra Package (includes navigation and visibility packages)", price: 999.99 }
+    ],
+    wheels: [
+        { id: 1, sizeType: "17-inch Pair Radial", price: 199.99 },
+        { id: 2, sizeType: "17-inch Pair Radial Black", price: 269.99 },
+        { id: 3, sizeType: "18-inch Pair Spoke Silver", price: 349.99 },
+        { id: 4, sizeType: "18-inch Pair Spoke Black", price: 379.99 },
+    ],
+    customOrders: [{
+        id: 1,
+        paintColorId: 3,
+        interiorId: 2,
+        techPackId: 3,
+        wheelId: 4,
+        timestamp: 1620059468223,
+    }],
+    //object that holds transient state (current user's input)
+    orderBuilder: {}
 
-    }
-    // set
+}
+
+// set
 export const setPaintColor = (id) => {
     database.orderBuilder.paintColorId = id
 }
